11f4c845026e35f5504c58d5a3c0a88863fa33a7
Bryan Lynn Welton III <weltontrain@gmail.com>
2023-04-06 21:58:44 -0600

    fixed last modified

--- bountiful/scripts/footer.js
+++ bountiful/scripts/footer.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Get the last modified date of the page
-    document.getElementById("last-modified").innerHTML = new Date(document.lastModified).toLocaleDateString();
-
-    // Get the current year
-    document.getElementById("current-year").innerHTML = new Date().getFullYear();
+  const options = { 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    second: 'numeric', 
+    hour12: true, 
+    month: '2-digit', 
+    day: '2-digit', 
+    year: 'numeric' 
+  };
+  const lastModified = new Date(document.lastModified).toLocaleString('en-US', options);
+  document.getElementById('last-modified').innerHTML = lastModified;
+  document.getElementById('current-year').innerHTML = new Date().getFullYear();
 });
